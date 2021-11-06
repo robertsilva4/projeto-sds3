a31de321755420d7226d243c82f174fa46ad4886
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-11-06 03:19:28 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,16 @@
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+function Routes() {
+    return (
+        <BrowserRouter>
+           <Route component = { Home }  path="/" exact />
+           <Route component = { Dashboard }  path="/dashboard" />
+       </BrowserRouter>
+    );
+}
+
+export default Routes;
